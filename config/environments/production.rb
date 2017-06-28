424c5a61b4aa808fe66db2b9daeb614f7ea85e8d
David Henry <david@decoybecoy.com>
2017-06-28 13:06:37 +0100

    Change to a domain I control

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'dropbox-accounts.herokuapp.com',
+    :domain         => 'dropbox.decoybecoy.com',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp

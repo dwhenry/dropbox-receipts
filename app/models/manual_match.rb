b7e7e0ddc4994a31e21edfc0b54dcaf0ffd0bab1
David Henry <david@decoybecoy.com>
2018-07-22 00:35:14 +0100

    Add some validations

--- app/models/manual_match.rb
+++ app/models/manual_match.rb
@@ -2,4 +2,9 @@ class ManualMatch < ApplicationRecord
   belongs_to :user
   has_many :lines, -> { where(source_type: 'ManualMatch') }, class_name: "BankLine", foreign_key: :source_id
   scope :without_source, -> { left_joins(:lines).where(bank_lines: { id: nil }) }
+
+  validates :amount, numericality: { greater_than: 0 }
+  validates :date, presence: true
+  validates :payment_type, presence: true
+  validates :payment_subtype, presence: true
 end

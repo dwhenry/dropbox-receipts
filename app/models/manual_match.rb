48b96e87e930889395449f5308bd6f060c02a1d6
David Henry <david@decoybecoy.com>
2018-07-22 01:44:25 +0100

    Fix negative matching

--- app/models/manual_match.rb
+++ app/models/manual_match.rb
@@ -1,7 +1,7 @@
 class ManualMatch < ApplicationRecord
   belongs_to :user
   has_many :lines, -> { where(source_type: 'ManualMatch') }, class_name: "BankLine", foreign_key: :source_id
-  scope :without_source, -> { left_joins(:lines).where("bank_lines.id is null OR manual_matches.amount NOT in (select sum(amount) from bank_lines as bl where bl.source_id = manual_matches.id and bl.source_type = 'ManualMatch')") }
+  scope :without_source, -> { left_joins(:lines).where("bank_lines.id is null OR manual_matches.amount NOT in (select sum(amount * -1) from bank_lines as bl where bl.source_id = manual_matches.id and bl.source_type = 'ManualMatch')") }
 
   validates :amount, numericality: { greater_than: 0 }
   validates :date, presence: true

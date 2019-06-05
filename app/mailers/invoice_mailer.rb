020ddf457f24f1f32721bc7b97eec8fcf9ba2b5b
David Henry <david@decoybecoy.com>
2019-06-05 17:25:42 +0100

    More user fixes

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -3,7 +3,7 @@ class InvoiceMailer < ApplicationMailer
     attachments[invoice.filename] = attachment
     mail(
       to: invoice.recipients.split(/\r?\n/),
-      subject: "Invoice for #{invoice.user.name} (#{invoice.tax_date.strftime('%d-%b-%Y')})",
+      subject: "Invoice for #{invoice.company.name} [#{invoice.company.user.name}] (#{invoice.tax_date.strftime('%d-%b-%Y')})",
       body: ''
     )
   end
--- app/worker/invoice_processor.rb
+++ app/worker/invoice_processor.rb
@@ -9,7 +9,7 @@ class InvoiceProcessor
     invoice = Invoice.find(invoice_id)
 
     path = invoice.build_path
-    client = Dropbox::Client.new(invoice.user.token)
+    client = Dropbox::Client.new(invoice.company.user.token)
 
     unless invoice.generated_at
       invoice.update!(generated_at: Time.now)
